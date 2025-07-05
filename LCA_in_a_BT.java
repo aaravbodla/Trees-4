f9c4535d3c9958a6ae41193be2aa09cb461260af
Aarav Bodla <147215351+aaravbodla@users.noreply.github.com>
2025-07-05 11:59:25 +0530

    Create LCA_in_a_BT.java

---
+++ LCA_in_a_BT.java
@@ -0,0 +1,15 @@
+// Time Comlexity -> O(n)
+// Space Complexity -> O(1)
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left == null && right == null) return null;
+        else if(left == null && right != null) return right;
+        else if(left != null && right == null) return left;
+        else return root;
+    }
+}
